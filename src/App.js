6545a5dfd17f49945799e569436b07c30839d479
kimba <skyqwn@gmail.com>
2022-08-01 15:02:48 +0900

    연습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let post = "강남 우동 맛집!";
+  let [title, setTitle] = useState([
+    "남자코트추천",
+    "강남 우동맛집",
+    "파이썬 독학",
+  ]);
+  let [like, setLike] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(false);
+  let [changeTitle, setChangeTitle] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+      <button
+        onClick={() => {
+          let copy = [...title];
+          setTitle(copy.sort());
+        }}
+      >
+        가나다순정렬
+      </button>
+      <button
+        onClick={() => {
+          let copy = [...title];
+          copy[0] = "여자코트추천";
+          setTitle(copy);
+        }}
+      >
+        글수정
+      </button>
+
+      {title.map((a, i) => {
+        return (
+          <div className="list">
+            <h4
+              onClick={() => {
+                setModal(true);
+                setChangeTitle(i);
+              }}
+            >
+              {title[i]}
+              <span
+                onClick={() => {
+                  let copyLike = [...like];
+                  copyLike[i] = copyLike[i] + 1;
+                  setLike(copyLike);
+                }}
+              >
+                👍
+              </span>
+              {like[i]}
+            </h4>
+            <p>7월 23일 발행</p>
+          </div>
+        );
+      })}
+      {modal === true ? (
+        <Modal changeTitle={changeTitle} setTitle={setTitle} title={title} />
+      ) : null}
     </div>
   );
 }
+
+const Modal = (props) => {
+  return (
+    <div className="modal">
+      <h4>{props.title[props.changeTitle]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button>글수정</button>
+    </div>
+  );
+};
 
 export default App;
